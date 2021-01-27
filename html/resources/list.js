0eb5213505578ff52bd001a09c623ed3bcec3533
caiosdeo <caiosdeo@hotmail.com>
2021-01-27 12:20:22 -0300

    changing index to work with exercises and work directories

--- html/resources/list.js
+++ html/resources/list.js
@@ -1,51 +1,26 @@
 var list = {
 	"en": {
-		"Projects": {
+		"Work": {
+			"Part 1":  {
+				"Kart": "html/kart1Preview",
+			},
+		},
+		"Exercises": {
 			"Basics":  {
-				"Basic Scene": "html/basicScene",
-				"Using the keyboard": "html/keyboardExampleInfo",
-				"Projection": "html/projectionInfo",
-				"Geometric Transformations 1": "html/geometricTransformationInfo",
-				"Geometric Transformations 2": "html/geometricTransformation2Info",
+				"Basic": "html/basicPreview",
+				"Camera": "html/cameraPreview",
+				"Transformations": "html/transformationsPreview",
 			},
 			"Animations":  {
-				"Basic": "html/animationInfo",
-				"Using External Objects": "html/animationExternalObjectsInfo",
+				"Animation 1": "html/animation1Preview",
+				"Animation 2": "html/animation2Preview",
 			},
 			"Geometries":  {
-				"Basic": "html/geometriesBasicInfo",
-				"Lathe": "html/geometriesLatheInfo",
-				"Extrusion": "html/geometriesExtrusionInfo",
-				"Convex": "html/geometriesConvexInfo",
-				"Loading External Files": "html/geometriesExternalObjectsInfo",
+				"Eolic Turbine": "html/eolicPreview"
 			},
 			"Lighting":  {
-				"Types of lights": "html/lightingTypesInfo",
-				"Types of materials": "html/lightingMaterialsInfo",
 			},
 			"Textures":  {
-				"Basic": "html/textureBasicInfo",
-				"Main Options": "html/textureOptionsInfo",
-				"Textured External Objects": "html/textureExternalObjectsInfo",
-			},
-		},
-		"Advanced Topics": {
-			"Raytracing":  {
-				"Example 1": "html/raytracingExample1Info",
-				"Example 2": "html/raytracingExample2Info",
-			},
-			"Augmented & Virtual Reality":  {
-				"VR - Primitives": "html/VR_BasicInfo",
-				"VR - Panorama": "html/VR_PanoramaInfo",
-				"VR - External Models": "html/VR_ExternalModelInfo",
-				"AR - Fiducial Marker": "html/AR_BasicInfo",
-			},
-		},
-		"Additional Examples":{
-			"Misc":  {
-				"Group Geometry": "html/exampleGroupInfo",
-				"Merge Geometry": "html/exampleMergeInfo",
-				"First Person": "html/exampleFirstPersonInfo",
 			},
 		},
 	},
